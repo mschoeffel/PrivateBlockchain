13bcdbc368fa05deaf7355446f79d0e2d261a66e
mschoeffel <michael.schoeffel@t-online.de>
2019-06-04 13:52:57 +0200

    Update Dependency Manager

--- src/main/java/logic/DependencyManager.java
+++ src/main/java/logic/DependencyManager.java
@@ -1,9 +1,14 @@
 package logic;
 
+import accounts.AccountStorage;
+import threads.Miner;
+
 public class DependencyManager {
 
     private static PendingTransactions pendingTransactions;
     private static Blockchain blockchain;
+    private static Miner miner;
+    private static AccountStorage accountStorage;
 
     public static PendingTransactions getPendingTransactions(){
         if(pendingTransactions == null){
@@ -17,6 +22,24 @@ public class DependencyManager {
             blockchain = new Blockchain();
         }
         return blockchain;
+    }
+
+    public static void injectBlockchain(Blockchain blockchain){
+        DependencyManager.blockchain = blockchain;
+    }
+
+    public static Miner getMiner(){
+        if(miner == null){
+            miner = new Miner();
+        }
+        return miner;
+    }
+
+    public static AccountStorage getAccountStorage(){
+        if(accountStorage == null){
+            accountStorage = new AccountStorage();
+        }
+        return accountStorage;
     }
 
 }
