73f32eaf3fac0a569c37e73cfbf452a4f233c23e
mschoeffel <michael.schoeffel@t-online.de>
2019-06-09 19:46:23 +0200

    main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,28 @@
 import models.Transaction;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import java.io.File;
 
 public class main {
 
     public static void main(String[] args){
-        Transaction transaction = new Transaction("this_is_the_sender", "this_is_the_reveiver", "this_is_my_data",10.0D, 22, 7.0D, 8.0D);
-        System.out.println(transaction.toString());
+        try{
+            Tomcat tomcat = new Tomcat();
+
+            String webappDirectory = new File("src/main/webapp").getAbsolutePath();
+
+            tomcat.setPort(8080);
+            Context context = tomcat.addWebapp("", webappDirectory);
+
+            Tomcat.addServlet(context, "blockchain", new ServletContainer(new Application()));
+            context.addServletMappingDecoded("/blockchain/api/*", "blcokchain");
+
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
