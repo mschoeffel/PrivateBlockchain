f4944d62ba18e0081ceab793f6f51249931c0e0d
mschoeffel <michael.schoeffel@t-online.de>
2019-06-25 19:52:13 +0200

    Commenting the code 3 ADD

--- src/main/java/threads/MinerListener.java
+++ src/main/java/threads/MinerListener.java
@@ -2,6 +2,11 @@ package threads;
 
 import models.Block;
 
+/**
+ * Interface for all the listeners to a miner
+ */
 public interface MinerListener {
+
+    //Method the miner will call when a new, valid block got mined
     void notifyNewBlock(Block block);
 }
