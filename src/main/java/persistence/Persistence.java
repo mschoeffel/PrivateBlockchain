a244a75e326858dd1c0f494d459a58d0235d32c5
mschoeffel <michael.schoeffel@t-online.de>
2019-06-26 21:39:16 +0200

    Commenting the code 4

--- src/main/java/persistence/Persistence.java
+++ src/main/java/persistence/Persistence.java
@@ -9,11 +9,19 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Class to handle persistent storage of the models
+ * At the moment this class is not used but if you wish to store the data consistent even if the network is stopped
+ * feel free to use it
+ */
 public class Persistence {
 
     private Charset encoding = StandardCharsets.UTF_8;
     private String path = "chains/";
 
+    /**
+     * Creates a new Persistence
+     */
     public Persistence(){
         File file = new File(path);
         if(!file.exists()){
@@ -21,7 +29,11 @@ public class Persistence {
         }
     }
 
-    public void wirteChain(Chain chain){
+    /**
+     * Writes the chain to the storage
+     * @param chain Chain to write
+     */
+    public void writeChain(Chain chain){
         int networkId = chain.getNetworkId();
         if(doesChainNotExist(networkId)){
             createChain(networkId);
@@ -34,21 +46,42 @@ public class Persistence {
         });
     }
 
+    /**
+     * Checks if a chain does not exist in the storage
+     * @param networkId Id of the blockchain network
+     * @return boolean if the chain does not exists
+     */
     private boolean doesChainNotExist(int networkId){
         File file = new File(getPathToChain(networkId));
         return !file.exists();
     }
 
+    /**
+     * Creates a chain storage file
+     * @param networkId Id of the blockchain network
+     */
     private void createChain(int networkId){
         File file = new File(getPathToChain(networkId));
         file.mkdir();
     }
 
+    /**
+     * Checks if a file with a block storage does not exist
+     * @param networkId Id of the blockchain network
+     * @param id Id of a block
+     * @return Boolean if the file doe not exist
+     */
     private boolean fileDoesNotExist(int networkId, String id){
         File file = new File(getPathToBlock(networkId, id));
         return !file.exists();
     }
 
+    /**
+     * Writes a new block storage file
+     * @param block Block to store
+     * @param networkId Id of the blockchain network
+     * @param id Id of the block
+     */
     private void writeBlock(Block block, int networkId, String id){
         File file = new File(getPathToBlock(networkId, id));
 
@@ -60,6 +93,11 @@ public class Persistence {
         }
     }
 
+    /**
+     * Reads a chain out of the storage
+     * @param networkId Id of the blockchain network
+     * @return new empty chain or, if storage exists, chain read from the storage
+     */
     public Chain readChain(int networkId){
         Chain chain = new Chain(networkId);
         if(doesChainExist(networkId)){
@@ -73,11 +111,21 @@ public class Persistence {
         return chain;
     }
 
+    /**
+     * Checks if a chain storage exists
+     * @param networkId Id of the blockchain network
+     * @return Boolean if the chain exists
+     */
     private boolean doesChainExist(int networkId){
         File file = new File(getPathToChain(networkId));
         return file.exists();
     }
 
+    /**
+     * Reads a block from a file
+     * @param file File to read from
+     * @return Block from the file or null
+     */
     private Block readBlock(File file){
         Block block = null;
 
@@ -90,10 +138,21 @@ public class Persistence {
         return block;
     }
 
+    /**
+     * Returns the storage path to a chain
+     * @param networkId Id of the blockchain network
+     * @return Path of the storage file of a chain
+     */
     private String getPathToChain(int networkId){
         return path + networkId;
     }
 
+    /**
+     * Returns the storage path to a block
+     * @param networkId Id of the blockchain network
+     * @param blockId Id of the block
+     * @return Path to the storage file of a block
+     */
     private String getPathToBlock(int networkId, String blockId){
         return path + networkId + "/" + blockId + ".json";
     }
