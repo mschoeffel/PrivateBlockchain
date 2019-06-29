d13b13d7e37b29a778978614d42dac847b5ad928
mschoeffel <michael.schoeffel@t-online.de>
2019-06-29 13:23:40 +0200

    Commenting the code 13

--- src/main/java/utils/SignatureUtil.java
+++ src/main/java/utils/SignatureUtil.java
@@ -17,16 +17,32 @@ import java.math.BigInteger;
 import java.security.*;
 import java.util.Arrays;
 
+/**
+ * Helper class to calculate and validate signatures
+ */
 public class SignatureUtil {
 
+    //Signature curve name
     private static final String CURVE_NAME = "secp256k1";
+    //Signature algorithm
     private static final String ALGORITHM = "ECDSA";
+    //Signature provider
     private static final String PROVIDER = "BC";
+    //Signature curve
     private static final X9ECParameters CURVE = SECNamedCurves.getByName(CURVE_NAME);
+    //Signature domain
     private static final ECDomainParameters DOMAIN =
             new ECDomainParameters(CURVE.getCurve(), CURVE.getG(), CURVE.getN(), CURVE.getH());
 
 
+    /**
+     * Verifies a given hash and signature depending on the public key
+     *
+     * @param hash      HAsh to verify
+     * @param signature Signature to verify
+     * @param publicKey Public key
+     * @return Boolean if the given hash and signature is valid or nit
+     */
     public static boolean verify(byte[] hash, byte[] signature, byte[] publicKey) {
         boolean result;
 
@@ -46,6 +62,11 @@ public class SignatureUtil {
         return result;
     }
 
+    /**
+     * Generates a new public/private key pair
+     *
+     * @return New public/private key pair
+     */
     public static KeyPair generateKeyPair() {
         KeyPair keyPair;
 
@@ -61,15 +82,33 @@ public class SignatureUtil {
         return keyPair;
     }
 
+    /**
+     * Saves a given keypair to a miner
+     *
+     * @param keyPair Keypair to save
+     * @param minerId Miner Id to save the keypair to
+     */
     public static void saveKeyPair(KeyPair keyPair, String minerId) {
         AccountPersistence accountPersistence = new AccountPersistence();
         accountPersistence.saveKeyPair(keyPair, minerId);
     }
 
+    /**
+     * Returns the coinbase from a given keypair
+     *
+     * @param keyPair Keypair to get the coinbase from
+     * @return Coinbase of the keypair
+     */
     public static byte[] getCoinbaseFromPublicKey(KeyPair keyPair) {
         return getCoinbaseFromPublicKey(keyPair.getPublic());
     }
 
+    /**
+     * Returns the coinbase from a public key
+     *
+     * @param key Public key to get the coinbase from
+     * @return Coinabse of the public key
+     */
     public static byte[] getCoinbaseFromPublicKey(PublicKey key) {
         return Arrays.copyOfRange(key.getEncoded(), 23, 88);
     }
